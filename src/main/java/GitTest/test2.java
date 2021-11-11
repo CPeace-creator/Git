68ebaab5519d4bb779dd045db1784e23a5a76d37
Chendream0128 <94037738+Chendream0128@users.noreply.github.com>
2021-11-11 11:31:44 +0800

    pull test

--- src/main/java/GitTest/test2.java
+++ src/main/java/GitTest/test2.java
@@ -5,5 +5,6 @@ public class test2 {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("push git");
+        System.out.println("pull git");
     }
 }
